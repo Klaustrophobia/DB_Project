51cbb3af9352780d5e8c56c6f23f95c60f078e5a
Klaustrophobia <Lalosempai2@gmail.com>
2024-05-03 00:10:29 -0600

    .

--- FRONTEND/js/app.js
+++ FRONTEND/js/app.js
@@ -9,3 +9,36 @@ sign_up_btn.addEventListener('click', () =>{
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
+
+new Vue({
+    el: '#app',
+    data: {
+      correo: '',
+      contrasenia: '',
+      mensaje: ''
+    },
+    methods: {
+      login() {
+        fetch('http://localhost:3000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ CORREO: this.correo, CONTRASENIA: this.contrasenia })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.status) {
+            this.mensaje = 'Login correcto';
+          } else {
+            this.mensaje = 'Credenciales incorrectas';
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          this.mensaje = 'Error en el inicio de sesión. Por favor, inténtelo de nuevo.';
+        });
+      }
+    }
+  });
+  
